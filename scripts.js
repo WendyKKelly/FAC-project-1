56ab0e9678ad6243a7192a09540ee8288f774644
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-21 14:41:34 -0700

    try again with date - external sheet plus formatting

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 console.log('Hello, world')
 
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+ 
+document.getElementById("today").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];

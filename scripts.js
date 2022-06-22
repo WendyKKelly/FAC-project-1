8726077f59f047e986955ecaa2fe264a604f8096
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-22 09:49:01 -0700

    interesting need to add one to month?

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ console.log('Hello, world')
 let today = new Date();
 var day = today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var date = today.getMonth()+'-'+(today.getDate()+1)+'-'+today.getFullYear();
+var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' and it is '+time + "o'clock";
+var dateTime = date+' and it is '+time + " o'clock";
  
 document.getElementById("displayDateTime").innerHTML = ' Today is ' + daylist[day] + dateTime;

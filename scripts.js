1c021f420bb6f9f9be975c11f9c2d7d2ce324ba5
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-22 11:28:07 -0700

    get rid of seconds

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let today = new Date();
 var day = today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
 var date = today.getMonth()+1+'-'+(today.getDate())+'-'+today.getFullYear();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var time = today.getHours() + ":" + today.getMinutes();
 var dateTime = date+' and it is '+time + " o'clock";
  
 document.getElementById("displayDateTime").innerHTML = ' Today is ' + daylist[day] + dateTime;

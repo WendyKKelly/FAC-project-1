292b16af015c213f7cc0f0b8dfecc18e8362ca01
Wendy Kelly <wendykelly@Wendys-iMac.local>
2022-06-22 09:36:30 -0700

    changed format for date

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 console.log('Hello, world')
 
-var today = new Date();
+let today = new Date();
 var day = today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
  
-document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+document.getElementById("displayDateTime").innerHTML = ' Today is' + daylist[day] + dateTime;
